a1cac68eb4fc5b961eaad63f3aa1ab74d5ee4f19
Robin Raymond <robin@hookflash.com>
2015-03-08 16:13:46 -0400

    - added yasm to prepare links script

--- prepare-links.sh
+++ prepare-links.sh
@@ -7,17 +7,30 @@ echo
 
 preparelink()
 {
-	if [ ! -d "$2" ]; then
-		echo ERROR: Link destination is not found \"$2\" !
+	if [ ! -d "$1" ]; then
+		echo ERROR: Path to link does not exist \"$1\" !
+	fi
+	pushd $1 > /dev/null
+	if [ ! -d "$3" ]; then
+		echo ERROR: Link destination is not found \"$3\" !
+		popd > /dev/null
 		exit -1
 	fi
-	if [ ! -h "$1" ]; then
-		echo Creating webrtc symbolic link \"$1\"...
-		ln -s $2 $1
+	if [ ! -h "$2" ]; then
+		echo Creating webrtc symbolic link \"$2\"...
+		ln -s $3 $2
+		if [ $? -ne 0 ]; then
+			failure=$?
+			echo Faield to create symbolic link
+			popd > /dev/null
+			exit $failure
+		fi
 	fi
+	popd > /dev/null
 }
 
-preparelink "build" "../webrtc-deps/webrtc-build/"
+preparelink "." "build" "../webrtc-deps/webrtc-build/"
+preparelink "third_party/yasm/source" "patched-yasm" "../../../../webrtc-deps/patched-yasm/"
 
 
 echo
