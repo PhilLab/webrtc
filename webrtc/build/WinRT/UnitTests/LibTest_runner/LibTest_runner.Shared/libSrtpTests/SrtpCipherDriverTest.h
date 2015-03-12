b38d4cfc64a13eb0a69ba13d83fcb54cbd7c84ac
Tomas Paseka <tomas.paseka@blackboard.com>
2015-03-12 10:19:01 +0100

    added more Srtp Cipher Driver tests

--- webrtc/build/WinRT/UnitTests/LibTest_runner/LibTest_runner.Shared/libSrtpTests/SrtpCipherDriverTest.h
+++ webrtc/build/WinRT/UnitTests/LibTest_runner/LibTest_runner.Shared/libSrtpTests/SrtpCipherDriverTest.h
@@ -3,26 +3,72 @@
 namespace libSrtpTests
 {
   //=============================================================================
-  //         class: CSrtpDriverTest
+  //         class: CSrtpCipherDriverValidationTest
   //   Description: class executes replay_driver test project, 
   //                see chromium\src\third_party\libsrtp\srtp_test_cipher_driver.vcxproj
   // History: 
   // 2015/02/27 TP: created
   //=============================================================================
-  class CSrtpCipherDriverTest :
+  class CSrtpCipherDriverValidationTest :
     public CLibSrtpTestBase
   {
   private:
-    AUTO_ADD_TEST(SingleInstanceTestSolutionProvider, CSrtpCipherDriverTest);
+    AUTO_ADD_TEST(SingleInstanceTestSolutionProvider, CSrtpCipherDriverValidationTest);
   protected:
     int InterchangeableExecute();
   public:
-    virtual ~CSrtpCipherDriverTest() {};
-    TEST_NAME_IMPL(CSrtpCipherDriverTest);
+    virtual ~CSrtpCipherDriverValidationTest() {};
+    TEST_NAME_IMPL(SrtpCipherDriverValidationTest);
     TEST_PROJECT_IMPL(srtp_test_cipher_driver);
     TEST_LIBRARY_IMPL(libSrtp);
   };
 
-  typedef std::shared_ptr<CSrtpCipherDriverTest> SpSrtpCipherDriverTest_t;
+  //=============================================================================
+  //         class: CSrtpCipherDriverTimingTest
+  //   Description: class executes replay_driver test project, 
+  //                see chromium\src\third_party\libsrtp\srtp_test_cipher_driver.vcxproj
+  // History: 
+  // 2015/02/27 TP: created
+  //=============================================================================
+  class CSrtpCipherDriverTimingTest :
+    public CLibSrtpTestBase
+  {
+  private:
+    AUTO_ADD_TEST(SingleInstanceTestSolutionProvider, CSrtpCipherDriverTimingTest);
+  protected:
+    int InterchangeableExecute();
+  public:
+    virtual ~CSrtpCipherDriverTimingTest() {};
+    TEST_NAME_IMPL(SrtpCipherDriverTimingTest);
+    TEST_PROJECT_IMPL(srtp_test_cipher_driver);
+    TEST_LIBRARY_IMPL(libSrtp);
+
+    virtual void InterchangeableVerifyResult();
+
+  };
+
+  //=============================================================================
+  //         class: CSrtpCipherDriverArrayTimingTest
+  //   Description: class executes replay_driver test project, 
+  //                see chromium\src\third_party\libsrtp\srtp_test_cipher_driver.vcxproj
+  // History: 
+  // 2015/02/27 TP: created
+  //=============================================================================
+  class CSrtpCipherDriverArrayTimingTest :
+    public CLibSrtpTestBase
+  {
+  private:
+    AUTO_ADD_TEST(SingleInstanceTestSolutionProvider, CSrtpCipherDriverArrayTimingTest);
+  protected:
+    int InterchangeableExecute();
+  public:
+    virtual ~CSrtpCipherDriverArrayTimingTest() {};
+    TEST_NAME_IMPL(SrtpCipherDriverArrayTimingTest);
+    TEST_PROJECT_IMPL(srtp_test_cipher_driver);
+    TEST_LIBRARY_IMPL(libSrtp);
+
+    virtual void InterchangeableVerifyResult();
+
+  };
 }
 
--- webrtc/build/WinRT/UnitTests/LibTest_runner/LibTest_runner.Windows/MainPage.xaml.cpp
+++ webrtc/build/WinRT/UnitTests/LibTest_runner/LibTest_runner.Windows/MainPage.xaml.cpp
@@ -25,13 +25,13 @@ using namespace Windows::UI::Xaml::Navigation;
 
 MainPage::MainPage()
 {
-	InitializeComponent();
+  InitializeComponent();
   OutputBox->Text = L"Tests results appears here\n";
 }
 
 void LibTest_runner::MainPage::RunAll_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
-  SpWStringReporter_t spStringReporter(new CWStringReporter(/*CWStringReporter::kPrintOutput*/));
+  SpWStringReporter_t spStringReporter(new CWStringReporter(CWStringReporter::kPrintOutput));
 
   // Update the UI to indicate test execution is in progress
   ProgressRingCtrl->IsActive = true;
@@ -42,8 +42,8 @@ void LibTest_runner::MainPage::RunAll_Click(Platform::Object^ sender, Windows::U
   create_task([this, ui, sender, spStringReporter]()
   {
     libSrtpTests::TestSolution::Instance().AddReporter(spStringReporter);
-    //libSrtpTests::TestSolution::Instance().Execute(L"SrtpAesCalcTest");
-    libSrtpTests::TestSolution::Instance().Execute();
+    libSrtpTests::TestSolution::Instance().Execute(L"SrtpCipherDriverTimingTest");
+    //libSrtpTests::TestSolution::Instance().Execute();
     libSrtpTests::TestSolution::Instance().GenerateReport();
   }).then([this, sender, spStringReporter]()
   {
