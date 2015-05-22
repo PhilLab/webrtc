7f8696976c2f3624ed4b03bd8965a436d8edc51c
David Campbell <dcampbell@macadamian.com>
2015-05-22 17:40:01 -0400

    Video renderer work in progress

--- webrtc/build/WinRT_gyp/Api/Media.h
+++ webrtc/build/WinRT_gyp/Api/Media.h
@@ -15,39 +15,40 @@ using namespace Windows::Media::Core;
 
 namespace webrtc_winrt_api
 {
-  class FrameBuffer
-  {
-  public:
-    FrameBuffer();
-    ~FrameBuffer();
-    void Initialize(int width, int height);
-    void SetFrame(const cricket::VideoFrame *frame);
-    void GenerateFrame(MediaStreamSourceSampleRequest ^ request);
-  private:
-    bool ConvertFrame(IMFMediaBuffer* mediaBuffer);
+  //class FrameBuffer
+  //{
+  //public:
+  //  FrameBuffer();
+  //  ~FrameBuffer();
+  //  void Initialize(int width, int height);
+  //  void SetFrame(const cricket::VideoFrame *frame);
+  //  void GenerateFrame(MediaStreamSourceSampleRequest ^ request);
+  //private:
+  //  bool ConvertFrame(IMFMediaBuffer* mediaBuffer);
 
-    int _width;
-    int _height;
-    Microsoft::WRL::ComPtr<IMFVideoSampleAllocator> _spSampleAllocator;
-    Microsoft::WRL::ComPtr<IMFMediaBuffer> _mediaBuffer;
-    CRITICAL_SECTION _critical;
-    int _stride;
-    BYTE* _buffer;
-    size_t _bufferSize;
-    ULONGLONG _timeStamp;
-  };
-  class RTCRenderer : public webrtc::VideoRendererInterface
-  {
-  public:
-    RTCRenderer(FrameBuffer& frameBuffer);
-    virtual ~RTCRenderer();
-    virtual void SetSize(int width, int height, int reserved);
-    virtual void RenderFrame(const cricket::VideoFrame *frame);
-    void SetMediaSource(MediaStreamSource^ mediaSource);
-  private:
-    FrameBuffer& _frameBuffer;
-    MediaStreamSource^ _mediaSource;
-  };
+  //  int _width;
+  //  int _height;
+  //  Microsoft::WRL::ComPtr<IMFVideoSampleAllocator> _spSampleAllocator;
+  //  Microsoft::WRL::ComPtr<IMFMediaBuffer> _mediaBuffer;
+  //  CRITICAL_SECTION _critical;
+  //  int _stride;
+  //  BYTE* _buffer;
+  //  size_t _bufferSize;
+  //  ULONGLONG _timeStamp;
+  //};
+
+  //class RTCRenderer : public webrtc::VideoRendererInterface
+  //{
+  //public:
+  //  RTCRenderer(FrameBuffer& frameBuffer);
+  //  virtual ~RTCRenderer();
+  //  virtual void SetSize(int width, int height, int reserved);
+  //  virtual void RenderFrame(const cricket::VideoFrame *frame);
+  //  void SetMediaSource(MediaStreamSource^ mediaSource);
+  //private:
+  //  FrameBuffer& _frameBuffer;
+  //  MediaStreamSource^ _mediaSource;
+  //};
 
   public interface class IMediaStreamTrack
   {
@@ -60,7 +61,7 @@ namespace webrtc_winrt_api
   {
   internal:
     MediaVideoTrack(rtc::scoped_refptr<webrtc::VideoTrackInterface> impl);
-    void OnSampleRequested(Windows::Media::Core::MediaStreamSource ^sender, Windows::Media::Core::MediaStreamSourceSampleRequestedEventArgs ^args);
+    //void OnSampleRequested(Windows::Media::Core::MediaStreamSource ^sender, Windows::Media::Core::MediaStreamSourceSampleRequestedEventArgs ^args);
   public:
     virtual ~MediaVideoTrack();
     virtual property String^ Kind { String^ get(); }
@@ -69,9 +70,9 @@ namespace webrtc_winrt_api
   internal:
     void SetRenderer(int width, int height, MediaStreamSource^ mediaSource);
   private:
-    FrameBuffer _frameBuffer;
+    //FrameBuffer _frameBuffer;
     rtc::scoped_refptr<webrtc::VideoTrackInterface> _impl;
-    rtc::scoped_ptr<RTCRenderer> _videoRenderer;
+    //rtc::scoped_ptr<RTCRenderer> _videoRenderer;
   };
 
   public ref class MediaAudioTrack sealed : public IMediaStreamTrack
@@ -105,8 +106,8 @@ namespace webrtc_winrt_api
   public:
     // TODO: Arguments
     IAsyncOperation<MediaStream^>^ GetUserMedia();
-    MediaStreamSource^ CreateMediaStreamSource(MediaVideoTrack^ track, uint32 width, uint32 height, uint32 framerate);
-    void OnStarting(Windows::Media::Core::MediaStreamSource ^sender, Windows::Media::Core::MediaStreamSourceStartingEventArgs ^args);
+    IMediaSource^ CreateMediaStreamSource(MediaVideoTrack^ track, uint32 width, uint32 height, uint32 framerate);
+    //void OnStarting(Windows::Media::Core::MediaStreamSource ^sender, Windows::Media::Core::MediaStreamSourceStartingEventArgs ^args);
   };
 
 }
--- webrtc/build/WinRT_gyp/Api/RTMediaStreamSource.cc
+++ webrtc/build/WinRT_gyp/Api/RTMediaStreamSource.cc
@@ -2,9 +2,38 @@
 
 namespace webrtc_winrt_api
 {
-
-  RTMediaStreamSource::RTMediaStreamSource()
+  IMediaSource^ RTMediaStreamSource::CreateMediaSource(MediaVideoTrack^ track, uint32 width, uint32 height, uint32 frameRate)
   {
+    auto videoProperties =
+      Windows::Media::MediaProperties::VideoEncodingProperties::CreateUncompressed(
+      Windows::Media::MediaProperties::MediaEncodingSubtypes::Bgra8, width, height);
+    auto videoDesc = ref new VideoStreamDescriptor(videoProperties);
+    videoDesc->EncodingProperties->FrameRate->Numerator = frameRate;
+    videoDesc->EncodingProperties->FrameRate->Denominator = 1;
+    videoDesc->EncodingProperties->Bitrate = (uint32)(videoDesc->EncodingProperties->FrameRate->Numerator*
+      videoDesc->EncodingProperties->FrameRate->Denominator * width * height * 4);
+    auto streamSource = ref new MediaStreamSource(videoDesc);
+    auto ret = ref new RTMediaStreamSource();
+    ret->_rtcRenderer = rtc::scoped_ptr<RTCRenderer>(new RTCRenderer(ret));
+    ret->_mediaStreamSource = streamSource;
+    ret->_mediaStreamSource->SampleRequested += ref new Windows::Foundation::TypedEventHandler<Windows::Media::Core::MediaStreamSource ^, 
+      Windows::Media::Core::MediaStreamSourceSampleRequestedEventArgs ^>(ret, &webrtc_winrt_api::RTMediaStreamSource::OnSampleRequested);
+    return ret;
+  }
+
+  RTMediaStreamSource::RTMediaStreamSource() : 
+    _stride(0), _buffer(nullptr), _sourceWidth(0), _sourceHeight(0)
+  {
+    InitializeCriticalSection(&_lock);
+  }
+
+  RTMediaStreamSource::~RTMediaStreamSource()
+  {
+    DeleteCriticalSection(&_lock);
+    if (_buffer != nullptr)
+    {
+      delete[] _buffer;
+    }
   }
 
   void RTMediaStreamSource::AddProtectionKey(IMediaStreamDescriptor^ streamDescriptor, const Array<uint8>^ keyIdentifier,
@@ -75,6 +104,110 @@ namespace webrtc_winrt_api
   Windows::Storage::FileProperties::MusicProperties^ RTMediaStreamSource::MusicProperties::get()
   {
     return _mediaStreamSource->MusicProperties;
+  }
+
+  Windows::Storage::Streams::IRandomAccessStreamReference^  RTMediaStreamSource::Thumbnail::get()
+  {
+    return _mediaStreamSource->Thumbnail;
+  }
+
+  void RTMediaStreamSource::Thumbnail::set(Windows::Storage::Streams::IRandomAccessStreamReference^ value)
+  {
+    _mediaStreamSource->Thumbnail = value;
+  }
+
+  Windows::Storage::FileProperties::VideoProperties^ RTMediaStreamSource::VideoProperties::get()
+  {
+    return _mediaStreamSource->VideoProperties;
+  }
+
+  EventRegistrationToken RTMediaStreamSource::Closed::add(TypedEventHandler<MediaStreamSource^, MediaStreamSourceClosedEventArgs^>^ value)
+  {
+    _mediaStreamSource->Closed += value;
+  }
+
+  void RTMediaStreamSource::Closed::remove(Windows::Foundation::EventRegistrationToken token)
+  {
+    _mediaStreamSource->Closed -= token;
+  }
+
+  EventRegistrationToken RTMediaStreamSource::Paused::add(TypedEventHandler<MediaStreamSource^, Object^>^ value)
+  {
+    _mediaStreamSource->Paused += value;
+  }
+
+  void RTMediaStreamSource::Paused::remove(Windows::Foundation::EventRegistrationToken token)
+  {
+    _mediaStreamSource->Paused -= token;
+  }
+
+  EventRegistrationToken RTMediaStreamSource::SampleRequested::add(TypedEventHandler<MediaStreamSource^, MediaStreamSourceSampleRequestedEventArgs^>^ value)
+  {
+    _mediaStreamSource->SampleRequested += value;
+  }
+
+  void RTMediaStreamSource::SampleRequested::remove(Windows::Foundation::EventRegistrationToken token)
+  {
+    _mediaStreamSource->SampleRequested -= token;
+  }
+
+  EventRegistrationToken RTMediaStreamSource::Starting::add(TypedEventHandler<MediaStreamSource^, MediaStreamSourceStartingEventArgs^>^ value)
+  {
+    _mediaStreamSource->Starting += value;
+  }
+
+  void RTMediaStreamSource::Starting::remove(Windows::Foundation::EventRegistrationToken token)
+  {
+    _mediaStreamSource->Starting -= token;
+  }
+
+  EventRegistrationToken RTMediaStreamSource::SwitchStreamsRequested::add(TypedEventHandler<MediaStreamSource^, MediaStreamSourceSwitchStreamsRequestedEventArgs^>^ value)
+  {
+    _mediaStreamSource->SwitchStreamsRequested += value;
+  }
+
+  void RTMediaStreamSource::SwitchStreamsRequested::remove(Windows::Foundation::EventRegistrationToken token)
+  {
+    _mediaStreamSource->SwitchStreamsRequested -= token;
+  }
+
+  RTMediaStreamSource::RTCRenderer::RTCRenderer(RTMediaStreamSource^ streamSource) : _streamSource(streamSource)
+  {
+  }
+
+  RTMediaStreamSource::RTCRenderer::~RTCRenderer()
+  {
+  }
+
+  void RTMediaStreamSource::RTCRenderer::SetSize(int width, int height, int reserved)
+  {
+  }
+
+  void RTMediaStreamSource::RTCRenderer::RenderFrame(const cricket::VideoFrame *frame)
+  {
+    auto stream = _streamSource.Resolve<RTMediaStreamSource>();
+    if (stream != nullptr)
+    {
+      stream->ProcessReceivedFrame(frame);
+    }
+  }
+
+  void RTMediaStreamSource::OnSampleRequested(MediaStreamSource ^sender, MediaStreamSourceSampleRequestedEventArgs ^args)
+  {
+    EnterCriticalSection(&_lock);
+    LeaveCriticalSection(&_lock);
+  }
+
+  void RTMediaStreamSource::ProcessReceivedFrame(const cricket::VideoFrame *frame)
+  {
+    EnterCriticalSection(&_lock);
+    if (_buffer == nullptr)
+    {
+      // Not ready for frames yet
+      LeaveCriticalSection(&_lock);
+      return;
+    }
+    LeaveCriticalSection(&_lock);
   }
 
 }
