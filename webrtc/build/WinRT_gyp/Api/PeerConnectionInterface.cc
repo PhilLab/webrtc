727f8725d615304610f226f8a342acbd0b38825f
Adam Bartha <abartha@macadamian.com>
2015-06-05 17:57:22 +0300

    Fixed codeingstyle and naming issues

--- webrtc/build/WinRT_gyp/Api/PeerConnectionInterface.cc
+++ webrtc/build/WinRT_gyp/Api/PeerConnectionInterface.cc
@@ -43,8 +43,8 @@ rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>
   gPeerConnectionFactory;
   // The worker thread for webrtc.
   rtc::Thread gThread;
-  rtc::TraceLog tl;
-  rtc::scoped_ptr<rtc::LoggingServer> ls;
+  rtc::TraceLog gTraceLog;
+  rtc::scoped_ptr<rtc::LoggingServer> gLoggingServer;
 }  // namespace globals
 
 RTCIceCandidate::RTCIceCandidate() {
@@ -366,56 +366,48 @@ void WebRTC::Initialize(Windows::UI::Core::CoreDispatcher^ dispatcher) {
     rtc::InitializeSSL();
 
     globals::gPeerConnectionFactory = webrtc::CreatePeerConnectionFactory();
-    webrtc::SetupEventTracer(&WebRTC::GetCategoryGroupEnabled, &WebRTC::AddTraceEvent);
+    webrtc::SetupEventTracer(&WebRTC::GetCategoryGroupEnabled,
+      &WebRTC::AddTraceEvent);
   });
 }
 
-bool WebRTC::IsTracing()
-{
-  return globals::tl.IsTracing();
+bool WebRTC::IsTracing() {
+  return globals::gTraceLog.IsTracing();
 }
 
-void WebRTC::StartTracing()
-{
-  globals::tl.StartTracing();
+void WebRTC::StartTracing() {
+  globals::gTraceLog.StartTracing();
 }
 
-void WebRTC::StopTracing()
-{
-  globals::tl.StopTracing();
+void WebRTC::StopTracing() {
+  globals::gTraceLog.StopTracing();
 }
 
-bool WebRTC::SaveTrace(Platform::String^ filename)
-{
-  std::wstring filename_ws(filename->Begin());
-  std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-  std::string filename_mb = converter.to_bytes(filename_ws);
-  return globals::tl.Save(filename_mb);
+bool WebRTC::SaveTrace(Platform::String^ filename) {
+  std::string filenameStr = FromCx(filename);
+  return globals::gTraceLog.Save(filenameStr);
 }
 
-bool WebRTC::SaveTrace(Platform::String^ host, int port)
-{
-  std::wstring host_ws(host->Begin());
-  std::string host_mb(host_ws.begin(), host_ws.end());
-  return globals::tl.Save(host_mb, port);
+bool WebRTC::SaveTrace(Platform::String^ host, int port) {
+  std::string hostStr = FromCx(host);
+  return globals::gTraceLog.Save(hostStr, port);
 }
 
-void WebRTC::EnableLogging(LogLevel level)
-{
+void WebRTC::EnableLogging(LogLevel level) {
   rtc::SocketAddress sa(INADDR_ANY, 47003);
-  globals::ls = rtc::scoped_ptr<rtc::LoggingServer>(new rtc::LoggingServer());
-  globals::ls->Listen(sa, (int)level);
+  globals::gLoggingServer = rtc::scoped_ptr<rtc::LoggingServer>(
+    new rtc::LoggingServer());
+  globals::gLoggingServer->Listen(sa, static_cast<int>(level));
   LOG(LS_INFO) << "WebRTC logging enabled";
 }
 
-void WebRTC::DisableLogging()
-{
+void WebRTC::DisableLogging() {
   LOG(LS_INFO) << "WebRTC logging disabled";
-  globals::ls.reset();
+  globals::gLoggingServer.reset();
 }
 
-const unsigned char* /*__cdecl*/ WebRTC::GetCategoryGroupEnabled(const char* category_group)
-{
+const unsigned char* /*__cdecl*/ WebRTC::GetCategoryGroupEnabled(
+  const char* category_group) {
   return reinterpret_cast<const unsigned char*>("webrtc");
 }
 
@@ -427,9 +419,9 @@ void __cdecl WebRTC::AddTraceEvent(char phase,
   const char** arg_names,
   const unsigned char* arg_types,
   const unsigned long long* arg_values,
-  unsigned char flags)
-{
-  globals::tl.Add(phase, category_group_enabled, name, id, num_args, arg_names, arg_types, arg_values, flags);
+  unsigned char flags) {
+  globals::gTraceLog.Add(phase, category_group_enabled, name, id,
+    num_args, arg_names, arg_types, arg_values, flags);
 }
 
 }  // namespace webrtc_winrt_api
