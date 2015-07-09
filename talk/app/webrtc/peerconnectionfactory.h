400a3f6161ab69fccf229ff78b18a03f2619fcbc
Toma Becea <tbecea@macadamian.com>
2015-07-09 11:26:22 +0300

    Removed useless scope operator.

--- talk/app/webrtc/peerconnectionfactory.h
+++ talk/app/webrtc/peerconnectionfactory.h
@@ -83,7 +83,7 @@ class PeerConnectionFactory : public PeerConnectionFactoryInterface {
 
  protected:
   PeerConnectionFactory();
-  PeerConnectionFactory::PeerConnectionFactory(
+  PeerConnectionFactory(
       cricket::WebRtcVideoEncoderFactory* video_encoder_factory,
       cricket::WebRtcVideoDecoderFactory* video_decoder_factory);
   PeerConnectionFactory(
